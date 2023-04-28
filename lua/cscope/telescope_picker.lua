e831815f1996c98182eda3c1be2b3f47ad490e73
Florian Larysch <fl@n621.de>
2023-04-28 20:19:10 +0200

    telescope_picker: add dirty displayer support

--- lua/cscope/telescope_picker.lua
+++ lua/cscope/telescope_picker.lua
@@ -3,19 +3,39 @@ local M = {}
 local pickers = require("telescope.pickers")
 local finders = require("telescope.finders")
 local config = require("telescope.config")
+local make_entry = require("telescope.make_entry")
+local entry_display = require "telescope.pickers.entry_display"
+local utils = require("telescope.utils")
 
-local entry_maker = function(entry)
-	return {
-		value = entry,
-		display = string.gsub(entry["filename"], vim.loop.cwd() .. "/", "")
-			.. ":"
-			.. entry["lnum"]
-			.. " | "
-			.. entry["text"],
-		ordinal = entry["filename"],
-		path = entry["filename"],
-		lnum = tonumber(entry["lnum"]),
+local make_from_cscope = function(entry)
+	local items = {
+		{ width = 30 },
+		{ remaining = true },
 	}
+	local displayer = entry_display.create { separator = "▏", items = items }
+
+	local make_display = function(entry)
+		local input = {}
+		table.insert(input, string.format("%s:%s", utils.transform_path({}, entry.filename), entry.lnum))
+
+		local text = entry.text
+		text = text:gsub(".* | ", "")
+		table.insert(input, text)
+
+		return displayer(input)
+	end
+
+	return function(entry)
+		return make_entry.set_default_entry_mt({
+				value = entry,
+				ordinal = entry.filename .. " " .. entry.text,
+				display = make_display,
+
+				filename = entry.filename,
+				lnum = tonumber(entry.lnum),
+				text = entry.text,
+			}, {})
+	end
 end
 
 local finder = nil
@@ -23,9 +43,9 @@ local prompt_title = nil
 
 M.prepare = function(cscope_parsed_output, telescope_title)
 	finder = finders.new_table({
-		results = cscope_parsed_output,
-		entry_maker = entry_maker,
-	})
+			results = cscope_parsed_output,
+			entry_maker = make_from_cscope()
+		})
 
 	prompt_title = telescope_title
 end
